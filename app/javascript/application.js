1493d2df3611170c3b9ac113608eab915c89710b
faezehzand <faezezand1365@gmail.com>
2025-02-02 20:20:39 +0100

    Remove all files from the main branch

--- app/javascript/application.js
+++
@@ -1,7 +0,0 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "bootstrap"
-import "filepond"
-import "custom/custom"
-import "filepond-plugin-image-preview"
-import "filepond-plugin-file-validate-type"
-
--- db/migrate/20241011170536_add_bio_and_is_private_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddBioAndIsPrivateToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :bio, :text
-    add_column :users, :private, :boolean, default: true
-  end
-end
